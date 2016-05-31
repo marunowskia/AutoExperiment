cc38f23a78c53dac3f976a0f92b5f9ab54e9e799
Alexander Marunowski <marunowskia@gmail.com>
2016-05-31 02:28:42 -0400

    added function to find the first EXPERIMENT_GROUP which has not been completed

---
+++ src/psql/developer_utils/create_functions/GET_ACTIVE_EXPERIMENT_GROUP.sql
@@ -0,0 +1,26 @@
+﻿CREATE OR REPLACE FUNCTION get_active_experiment_group()
+RETURNS bigint AS
+$$
+BEGIN
+	RETURN
+	(
+		SELECT
+			EG.ID
+		FROM
+			EXPERIMENT_GROUP EG
+		LEFT JOIN
+			EXPERIMENT E
+		ON
+			EG.ID = E.EXPERIMENT_GROUP_ID
+		GROUP BY
+			EG.ID, 
+			EG.MAX_EXPERIMENT_COUNT 
+		HAVING
+			COUNT(*) < EG.MAX_EXPERIMENT_COUNT
+			-- this approach is not well suited to large scale experiment databases (1 million experiments or more)
+			-- may want replace this solution with an approach that decrements a "REMAINING_EXPERIMENTS" column on the EXPERIMENT_GROUP table
+		LIMIT 1
+	);	
+END
+$$
+LANGUAGE plpgsql
