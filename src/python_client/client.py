013f53e286bf0acb27ab961b7483da70786a51dc
Alexander Marunowski <marunowskia@gmail.com>
2016-06-10 00:31:30 -0400

    typos :(. Fixed

--- src/python_client/client.py
+++ src/python_client/client.py
@@ -1,14 +1,16 @@
 import dbUtil
 
-db = dbUtil.getDefaultDb();
+db = dbUtil.getDefaultDatabase();
 
 while True:
 	experimentId = db.getNextExperiment()
 	if experimentId is not None:
+		print(experimentId)
 		experimentParams = db.getExperimentParams(experimentId)
 		experimentScript = db.getExperimentScript(experimentId)
-		exec experimentScript #creates function called runExperiment(experimentParams)
+
+		exec experimentScript #creates function called runExperiment(experimentParams)		
 		score, detail = runExperiment(experimentParams)
-		db.recordResult(experimentId, score, detail)
+		db.recordExperimentResult(experimentId, score, detail)
 	else:
 		break
