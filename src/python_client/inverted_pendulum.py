09b67600563bfb1679fd9b9f6666d3e128393e90
Alexander Marunowski <marunowskia@gmail.com>
2016-06-12 22:03:05 -0400

    adding skeleton for the new demo to compel me to write the darn thing.

---
+++ src/python_client/inverted_pendulum.py
@@ -0,0 +1,16 @@
+# INVERTED PENDULUM DEMO
+# This is included as the simplest yet most perverse example I could think of for a learnable, highly non-convex control problem.
+# The demo is comprised of a double inverted pendulum simulation, which receives control input from a neural network.
+
+# That the system successfully learns to control this system reasonably well is a powerful 
+# demonstration of the general applicability of simple evolutionary search algorithms.
+
+
+
+
+
+
+
+def runExperiment(experimentParameters):
+	# TODO
+	# I know, right?
