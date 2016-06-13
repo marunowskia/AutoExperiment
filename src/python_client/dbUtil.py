1c9ae085622da5b0f8a0028b1ecf0521db12133d
Alexander Marunowski <marunowskia@gmail.com>
2016-06-12 22:04:05 -0400

    changed the exception handling approach. The system now performs positive verification of database responses, rather than just handling exceptions

--- src/python_client/dbUtil.py
+++ src/python_client/dbUtil.py
@@ -6,32 +6,28 @@ class DbUtil:
 		self.cursor = self.conn.cursor()
 
 	def getNextExperiment(self):
-#		try:
 		self.cursor.execute('select get_next_experiment()')
-		return self.cursor.fetchall()[0][0]
-#		except:
-#			print("Unexpected error:", sys.exc_info()[0])
-#			return None
+
+		result = self.cursor.fetchall()
+		if len(result) == 1 and len(result[0]) == 1:
+			return result[0][0]
+		return None
 
 	def getExperimentScript(self, experimentId):
-#		try:
 		self.cursor.execute('select get_experiment_script(%s)', (experimentId,))
-		return self.cursor.fetchall()[0][0]
-#		except:
-#			print("Unexpected error:", sys.exc_info()[0])
-#			return None
+		result = self.cursor.fetchall()
+		if len(result) == 1 and len(result[0]) == 1:
+			return result[0][0]
+		return None
+
 
 	def getExperimentParams(self, experimentId):
-#		try:
 		self.cursor.execute('select * from get_experiment_parameters(%s)', (experimentId,))
 		experimentParams = self.cursor.fetchall()
 		parameterData = {}
 		for listElement in experimentParams:
 			parameterData[listElement[0]] = listElement[1]
 		return parameterData
-#		except:
-#			print("Unexpected error:", sys.exc_info()[0])
-#			return None
 
 	def recordExperimentResult(self, experimentId, score, detail):
 		self.cursor.execute('select record_experiment_result(%s, %s, %s)', (experimentId, score, detail))
